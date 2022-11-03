aa52bc1ad4550ecfa791da05654fe78bf5f255d7
mendusekaterina <arhipenko.katja@gmail.com>
2022-11-03 17:20:37 +0100

    finished 1st task JS

---
+++ lesson1/js1.js
@@ -0,0 +1,55 @@
+let s1 = 'hello';
+console.log(s1);
+let s2 = 'owu';
+console.log(s2);
+let s3 = 'com';
+console.log(s3);
+let s4 = 'ua';
+console.log(s4);
+let s5 = 1;
+console.log(s5);
+let s6 = 10;
+console.log(s6);
+let s7 = -999;
+console.log(s7);
+let s8 = 123;
+console.log(s8);
+const PI = 3.14;
+console.log(PI);
+let s10 = 2.7;
+console.log(s10);
+let s11 = 16;
+console.log(s11);
+let s12 = true;
+console.log(s12);
+let s13 = false;
+console.log(s13);
+
+console.log(s1, s2, s3, s4,
+    s5, s6, s7, s8, PI, s10, s11, s12, s13);
+
+let firstName = 'Kateryna';
+let middleName = 'Yuriivna';
+let lastName = 'Mendus';
+let person = firstName + ' ' + middleName + ' ' + lastName
+console.log(person);
+person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let p1 = prompt('Your name');
+let p2 = prompt('Your surname');
+let p3 = prompt('Your age');
+console.log(p1 + ' ' + p2 + ' ,' + p3);
+console.log(+p3);
+
+
+
+
