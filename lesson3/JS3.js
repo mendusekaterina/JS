78ec07ab2c24a020566c79ed9805fe84bfeeda5c
mendusekaterina <arhipenko.katja@gmail.com>
2022-11-08 18:16:33 +0100

    finished third Home Work with comments

--- lesson3/JS3.js
+++ lesson3/JS3.js
@@ -1,3 +1,7 @@
+// - Є змінна х, якій ви надаєте довільне числове значення.
+// Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
 let x = +prompt('enter the X');
 if (x !== 0) {
     console.log('correct');
@@ -5,6 +9,9 @@ if (x !== 0) {
     console.log('incorrect');
 }
 
+// - Дано змінну time яка рівна числу від 0 до 59.
+// Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
 
 let time = +prompt('enter time');
 if (time >= 0 &&
@@ -30,6 +37,33 @@ else {
     console.log ('not a time')
 }
 
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+let day = +prompt('enter day');
+if (day >= 1 &&
+    day <= 10) {
+    console.log('first decade');
+}
+
+else if
+(day > 10 &&
+    day <= 20) {
+    console.log('second decade');
+}
+
+else if (day > 20 &&
+    day <= 31) {
+    console.log('third decade');
+}
+else {
+    console.log ('the date is not exist')
+}
+
+// - Скласти розклад на тиждень за домопоги switch.
+// Користувач вводить порядковий номер дня тижня і на екрані відображається інфа,
+// що заплановано на цей день (можна
+// замість плану на день, назву дня англійською).
+
 switch (prompt('enter the number of the day of the week'))
 {
     case '1':
@@ -54,8 +88,12 @@ switch (prompt('enter the number of the day of the week'))
         console.log ('Sunday');
         break;
     default:
-        console.log('the day does not exist');
+        console.log('the day of week does not exist');
 }
+
+// - Користувач вводить або має два числа.
+//  Потрібно знайти та вивести максимальне число з тих двох .
+//  Також потрібно врахувати коли введені рівні числа.
 
 let a=+prompt('enter a number A');
 let b=+prompt('enter a number B');
@@ -72,6 +110,10 @@ else if (b===a){
 else {
     console.log('incorrect input')
 }
+
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно).
+// Напишіть код який, за допомоги  оператора || буде присвоювати змінній х значення "default"
+// якщо значення змінної х являється falsy (хибноподыбне, тобто кастується до false)
 
 let z=!!+prompt('enter any X') || 'default'
 console.log(z);
